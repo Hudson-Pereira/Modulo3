6b3708daddc273aa9f5a4bd06e4bbcc4e907077f
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-10-26 22:59:51 -0300

    fim da aula 2 do m3

---
+++ Aula2-M3-CL/Calc.js
@@ -0,0 +1,6 @@
+const calculadora = require("./FuncitonCalc");
+
+console.log(calculadora.soma(5, 3));
+console.log(calculadora.menos(5, 3));
+console.log(calculadora.vezes(5, 3));
+console.log(calculadora.div(5, 3));
